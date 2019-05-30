8b3cbbdf1cd5e206c0c1e2a475b5b84b0fc23ccc
FlaxMine <FlaxMine@users.noreply.github.com>
2019-05-30 18:50:44 +0300

    initial commit

--- admin/adminEdit.php
+++ admin/adminEdit.php
@@ -2,8 +2,11 @@
 
     session_start();
 
+    require "../database/connection.php";
+    $link = ConnectionDB();
+
     $errors = "";
-    $successfully = "";
+    $succesfull = "";
 
     if(isset($_REQUEST['Download'])){
 
@@ -20,19 +23,71 @@
 
                     $time = time();
         
-                    if(move_uploaded_file($tmp_name_0, "../img/book/$time" .$name_file_0) 
-                                    && move_uploaded_file($tmp_name_1, "../img/author_img/$time" .$name_file_1)){
-                        $successfully = "Files was uploaded successfully!";
+                    if(move_uploaded_file($tmp_name_1, "../img/book/$time" .$name_file_1) 
+                                    && move_uploaded_file($tmp_name_0, "../img/author_img/$time" .$name_file_0)){
+                        $succesfull = "Files was uploaded successfully!";
                     }
                     else { $errors = "Error loading!";}
                 }
                 else { $errors = "File size too large!"; }
             }
             else { $errors = "Not valid file type!"; }       
-        }
+        }  
     }
-?>
 
+    
+
+    if(isset($_REQUEST['Insert'])){
+
+        $nameBook = $_REQUEST['nameBook'];
+        $aboutBook = $_REQUEST['aboutBook'];
+        $BookImg = $_REQUEST['BookImg'];
+
+        $authorBook = $_REQUEST['authorBook'];
+        $authorYear = $_REQUEST['authorYear'];
+        $AuthorImg = $_REQUEST['AuthorImg'];
+
+        $aboutAuthor = $_REQUEST['aboutAuthor'];
+
+        $genreBook = $_REQUEST['genreBook'];
+
+        $DateWrite = $_REQUEST['DateWrite'];
+        $YearPublish = $_REQUEST['YearPublish'];
+        $QuantityPage = $_REQUEST['QuantityPage'];
+
+        $query_book = "INSERT INTO book (name, img) VALUES ('$nameBook', '$BookImg')";
+        $query_author = "INSERT INTO author (FIO, year) VALUES ('$authorBook', '$authorYear')";
+
+        $result_query_book = mysqli_query($link, $query_book);
+        $result_query_author = mysqli_query($link, $query_author);
+
+        $query_last_id = "SELECT MAX(id) as LastID FROM book";
+        $result = mysqli_query($link, $query_last_id);
+        $id_result = mysqli_fetch_array($result);
+        $id = $id_result['LastID'];
+
+        $query_id_genre = "SELECT id FROM genre WHERE genre = '$genreBook'";
+        $result_id_genre = mysqli_query($link, $query_id_genre);
+        $id_genre = mysqli_fetch_array($result_id_genre);
+        $id2 = $id_genre['id'];
+
+        $query_attribute = "INSERT INTO attribute (id_genre, id_book) VALUES ('$id2', '$id')";
+        $query_about_author = "INSERT INTO information_author (id_author, about_author, img) VALUES ('$id', '$aboutAuthor', '$AuthorImg')"; 
+        $query_detailed_information = "INSERT INTO detailed_information (year_edition, year_write, quantity_page, id_book, description) 
+                                        VALUES ('$YearPublish', '$DateWrite', '$QuantityPage', '$id', '$aboutBook')";
+
+        $result_query_about_author = mysqli_query($link, $query_about_author);
+        $result_query_detailed_information = mysqli_query($link, $query_detailed_information);
+        $result_query_attribute = mysqli_query($link, $query_attribute);
+
+        if($result_query_book == TRUE && $result_query_author == TRUE
+            && $result_query_about_author == TRUE && $result_query_detailed_information == TRUE
+            && $result_query_attribute == TRUE){
+            echo "Данные успешно добавлены!";
+        }else { echo "Ошибка!";}
+    }
+    
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -104,12 +159,12 @@
                 <?php 
                     if(!empty($errors)){
                         echo $errors;
-                    }else {echo $successfully; }
+                    }else { echo $succesfull; }
                 ?>
             </div>
         </div><br>
 
-        <form action="registration.php" method="POST">
+        <form action="adminEdit.php" method="POST">
             <div class="admin_edit">
                 <div class="nameBook">
                     <input type="text" class="input_search" name="nameBook" placeholder="NameBook">                   
@@ -117,6 +172,10 @@
                 
                 <div class="authorBook">
                     <input type="text" class="input_search" name="authorBook" placeholder="authorBook">
+                </div>
+
+                <div class="authorYear">
+                    <input type="text" class="input_search" name="authorYear" placeholder="authorYear">
                 </div>
 
                 <div class="genreBook">
@@ -135,6 +194,20 @@
                     <input type="text" class="input_search" name="QuantityPage" placeholder="QuantityPage">
                 </div>
 
+                <div class="AuthorImg">
+                    <?php 
+                        $file_name = empty($_FILES['picture']['name'][0]) ? "" : $time .$_FILES['picture']['name'][0];
+                    ?>
+                    <input type="hidden" class="input_search" name="AuthorImg" placeholder="AuthorImg" value="img/book/<?php echo $file_name; ?>">
+                </div>
+
+                <div class="BookImg">
+                    <?php 
+                        $file_name_2 = empty($_FILES['picture']['name'][1]) ? "" : $time .$_FILES['picture']['name'][1];
+                    ?>
+                    <input type="hidden" class="input_search" name="BookImg" placeholder="BookImg" value="img/author_img/<?php echo $file_name_2; ?>">
+                </div>            
+
                 <div class="aboutBook">
                     <p>About Book<Br>
                     <textarea name="aboutBook" cols="40" rows="10"></textarea></p>
@@ -146,7 +219,7 @@
                 </div>
 
                 <div class="Add">
-                    <input type="submit" class="button add" name="QuantityPage" placeholder="QuantityPage">
+                    <input type="submit" class="button add" name="Insert" placeholder="QuantityPage">
                 </div>
             </div>
         </form>
@@ -188,5 +261,6 @@
 
         </div>
     </footer>
+
 </body>
 </html>
