8082fb6c579581911cb81f80be4c20abd0f792cf
FlaxMine <FlaxMine@users.noreply.github.com>
2019-05-30 22:45:34 +0300

    admin update

---
+++ admin/update.php
@@ -0,0 +1,23 @@
+<?php
+
+    session_start();
+
+    require "../database/connection.php";
+    $link = ConnectionDB();
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>E-Library</title>
+</head>
+<body>
+    
+</body>
+</html>
